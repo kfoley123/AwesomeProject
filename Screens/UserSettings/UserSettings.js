1bcccf6f819c2e5ef60dce6ffa4eb9a9a42000f3
Kortney <kortney@iMac.local>
2023-02-28 11:05:32 -0400

    installed checkbox package and added checkboxes for notification message preferences in settings

--- Screens/UserSettings/UserSettings.js
+++ Screens/UserSettings/UserSettings.js
@@ -1,19 +1,48 @@
-import {
-    StyleSheet,
-    Modal,
-    Alert,
-    Text,
-    TextInput,
-    Pressable,
-    Button,
-    View,
-    Image,
-} from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Checkbox from "expo-checkbox";
 
 export default function UserSetting() {
+    const [SMSChecked, setSMSChecked] = useState(false);
+    const [isChecked, setChecked] = useState(true);
     return (
-        <View>
-            <Text> Settings!</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}> Notification Preferences</Text>
+            <View style={styles.checkboxContainer}>
+                <Checkbox
+                    style={styles.checkbox}
+                    value={SMSChecked}
+                    onValueChange={setSMSChecked}
+                    color={SMSChecked ? "#4630EB" : undefined}
+                />
+                <Text>SMS Message</Text>
+            </View>
+            <View style={styles.checkboxContainer}>
+                <Checkbox
+                    style={styles.checkbox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                />
+                <Text>Email</Text>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        textAlign: "center",
+        fontSize: 16,
+        paddingTop: 20,
+        paddingBottom: 10,
+    },
+    checkboxContainer: {
+        alignItems: "center",
+        flexDirection: "row",
+        paddingLeft: "35%",
+    },
+    checkbox: {
+        margin: 8,
+    },
+});
