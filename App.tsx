c7f634e2f063fbdd6de3882ef24e70eab4b64f4e
Kortney <kortney@iMac.local>
2023-03-02 11:52:10 -0400

    added state variable to keep track of if user is signed in and hard coded it to false for setup purposes. Made SignInScreen component and cinditionally rendered it vs the rest of the app depending on if the user is signed in or not.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pressable, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import {
@@ -9,6 +9,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import UserTabs from "./Screens/UserTabs/UserTabs";
 import UserProfile from "./Screens/UserProfile/UserProfile";
 import UserSettings from "./Screens/UserSettings/UserSettings";
+import SignInScreen from "./Screens/SignInScreen/SignInScreen";
 
 const Stack = createStackNavigator();
 
@@ -19,61 +20,78 @@ function getHeaderTitle(route) {
 }
 
 export default function App() {
+    const [isSignedIn] = useState(false);
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen
-                    name="Tabs"
-                    component={UserTabs}
-                    options={({ navigation, route }) => ({
-                        headerTitle: getHeaderTitle(route),
-                        headerRight: () => (
-                            <Pressable
-                                onPress={() => navigation.navigate("Profile")}
-                            >
-                                <Image
-                                    source={{
-                                        uri: "https://i.pinimg.com/originals/cc/2e/01/cc2e011cc5236801ee8fd6d2fc5dc2c5.jpg",
-                                    }}
-                                    style={{
-                                        width: 40,
-                                        height: 40,
-                                        borderColor: "black",
-                                        borderWidth: 0.5,
-                                        borderRadius: 100,
-                                        marginRight: 21,
-                                        marginBottom: 15,
-                                    }}
-                                />
-                            </Pressable>
-                        ),
-                        headerLeft: () => (
-                            <Pressable
-                                onPress={() => navigation.navigate("Settings")}
-                            >
-                                <Ionicons
-                                    name={"md-settings-outline"}
-                                    style={{
-                                        color: "dimgray",
-                                        fontSize: 25,
-                                        height: 38,
-                                        marginLeft: 20,
-                                    }}
-                                />
-                            </Pressable>
-                        ),
-                    })}
-                />
-                <Stack.Screen
-                    name="Profile"
-                    options={() => ({ headerBackTitle: "Back" })}
-                    component={UserProfile}
-                />
-                <Stack.Screen
-                    name="Settings"
-                    options={() => ({ headerBackTitle: "Back" })}
-                    component={UserSettings}
-                />
+                {isSignedIn ? (
+                    <>
+                        <Stack.Screen
+                            name="Tabs"
+                            component={UserTabs}
+                            options={({ navigation, route }) => ({
+                                headerTitle: getHeaderTitle(route),
+                                headerRight: () => (
+                                    <Pressable
+                                        onPress={() =>
+                                            navigation.navigate("Profile")
+                                        }
+                                    >
+                                        <Image
+                                            source={{
+                                                uri: "https://i.pinimg.com/originals/cc/2e/01/cc2e011cc5236801ee8fd6d2fc5dc2c5.jpg",
+                                            }}
+                                            style={{
+                                                width: 40,
+                                                height: 40,
+                                                borderColor: "black",
+                                                borderWidth: 0.5,
+                                                borderRadius: 100,
+                                                marginRight: 21,
+                                                marginBottom: 15,
+                                            }}
+                                        />
+                                    </Pressable>
+                                ),
+                                headerLeft: () => (
+                                    <Pressable
+                                        onPress={() =>
+                                            navigation.navigate("Settings")
+                                        }
+                                    >
+                                        <Ionicons
+                                            name={"md-settings-outline"}
+                                            style={{
+                                                color: "dimgray",
+                                                fontSize: 25,
+                                                height: 38,
+                                                marginLeft: 20,
+                                            }}
+                                        />
+                                    </Pressable>
+                                ),
+                            })}
+                        />
+                        <Stack.Screen
+                            name="Profile"
+                            options={() => ({ headerBackTitle: "Back" })}
+                            component={UserProfile}
+                        />
+                        <Stack.Screen
+                            name="Settings"
+                            options={() => ({ headerBackTitle: "Back" })}
+                            component={UserSettings}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Stack.Screen
+                            name="Sign In"
+                            component={SignInScreen}
+                            options={{ headerShown: false }}
+                        />
+                    </>
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ Screens/SignInScreen/SignInScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, SafeAreaView, Text, TouchableOpacity } from "react-native";
+
+export default function SignInScreen() {
+    return (
+        <SafeAreaView>
+            <View>
+                <TouchableOpacity>
+                    <Text>Sign in</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
