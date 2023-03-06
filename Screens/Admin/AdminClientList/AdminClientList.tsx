ec17a3355347c1e37c4542df5dfa934e0a892278
Kortney <kortney@iMac.local>
2023-03-06 14:44:15 -0400

    styled client view in Clients component and added functionality to the Add client button - a modal that adds a user to memory until next reload for time being. also updated some styles on user profile page to match clients page modal

--- Screens/Admin/AdminClientList/AdminClientList.tsx
+++ Screens/Admin/AdminClientList/AdminClientList.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { Text, ScrollView, View, StyleSheet, Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+    Text,
+    ScrollView,
+    View,
+    StyleSheet,
+    Image,
+    Modal,
+    Pressable,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function AdminClientList() {
-    const clientList = [
+    const [clientList, setClientList] = useState([
         {
             username: "Joel Client",
-            email: "bob@me.com",
+            email: "kortney_foley@hotmail.com",
             phoneNumber: "5555555556",
             appointments: [{ date: "dog" }, { date: "cat" }],
         },
@@ -46,13 +56,192 @@ export default function AdminClientList() {
             phoneNumber: "5555555558",
             appointments: [],
         },
-    ];
+        {
+            username: "Joel Client",
+            email: "bob@me.com",
+            phoneNumber: "5555555556",
+            appointments: [],
+        },
+        {
+            username: "Cammy White",
+            email: "fight@me.com",
+            phoneNumber: "5555555557",
+            appointments: [
+                { date: "dog" },
+                { date: "cat" },
+                { date: "dog" },
+                { date: "cat" },
+            ],
+        },
+        {
+            username: "Bob Ross",
+            email: "bob1@me.com",
+            phoneNumber: "5555555558",
+            appointments: [],
+        },
+        {
+            username: "Joel Client",
+            email: "bob@me.com",
+            phoneNumber: "5555555556",
+            appointments: [],
+        },
+        {
+            username: "Cammy White",
+            email: "fight@me.com",
+            phoneNumber: "5555555557",
+            appointments: [
+                { date: "dog" },
+                { date: "cat" },
+                { date: "dog" },
+                { date: "cat" },
+            ],
+        },
+        {
+            username: "Bob Ross",
+            email: "bob1@me.com",
+            phoneNumber: "5555555558",
+            appointments: [],
+        },
+    ]);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({});
+
+    const onSubmit = (data) =>
+        setClientList([...clientList, { ...data, appointments: [] }]);
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.addClientButton}>
-                <Ionicons name="person-add" size={24} color="black" />
-                <Text style={styles.addClientFont}>Add Client</Text>
-            </TouchableOpacity>
+            <View>
+                <TouchableOpacity
+                    onPress={() => setModalVisible(!modalVisible)}
+                    style={styles.addClientButton}
+                >
+                    <Ionicons name="person-add" size={24} color="black" />
+                    <Text style={styles.addClientFont}>Add Client</Text>
+                </TouchableOpacity>
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalContainer}>
+                            <Text style={styles.title}> Add Client</Text>
+
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                }}
+                                render={({
+                                    field: { onChange, onBlur, value },
+                                }) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        onBlur={onBlur}
+                                        placeholder={"Name"}
+                                        placeholderTextColor="#a9a9a9"
+                                        onChangeText={onChange}
+                                        autoCapitalize={"words"}
+                                        value={value}
+                                    />
+                                )}
+                                name="username"
+                            />
+                            {errors.username && <Text>Name is required.</Text>}
+
+                            <Controller
+                                control={control}
+                                rules={{
+                                    maxLength: 100,
+                                    required: true,
+                                    // eslint-disable-next-line no-useless-escape
+                                    pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                                }}
+                                render={({
+                                    field: { onChange, onBlur, value },
+                                }) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        onBlur={onBlur}
+                                        placeholder={"Email"}
+                                        placeholderTextColor="#a9a9a9"
+                                        onChangeText={onChange}
+                                        keyboardType={"email-address"}
+                                        value={value}
+                                    />
+                                )}
+                                name="email"
+                            />
+                            {errors.email && (
+                                <Text>A valid email is required.</Text>
+                            )}
+
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                    minLength: 9,
+                                    maxLength: 10,
+                                }}
+                                render={({
+                                    field: { onChange, onBlur, value },
+                                }) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        onBlur={onBlur}
+                                        placeholder={"Phone Number"}
+                                        placeholderTextColor="#a9a9a9"
+                                        keyboardType={"numeric"}
+                                        onChangeText={onChange}
+                                        value={value}
+                                    />
+                                )}
+                                name="phoneNumber"
+                            />
+                            {errors.phoneNumber && (
+                                <Text>Valid phone number is required.</Text>
+                            )}
+
+                            <View style={styles.buttonContainer}>
+                                <Pressable
+                                    style={styles.button}
+                                    onPress={() => {
+                                        setModalVisible(!modalVisible);
+                                        reset();
+                                    }}
+                                >
+                                    <Text style={styles.buttonText}>
+                                        Cancel
+                                    </Text>
+                                </Pressable>
+
+                                <Pressable
+                                    style={styles.button}
+                                    disabled={Object.keys(errors).length > 0}
+                                    onPress={() => {
+                                        setModalVisible(!modalVisible);
+                                        handleSubmit(onSubmit)();
+                                        reset();
+                                    }}
+                                >
+                                    <Text style={styles.buttonText}>Done</Text>
+                                </Pressable>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+            </View>
 
             <ScrollView style={styles.clientListContainer}>
                 {clientList.map((client, i) => {
@@ -64,41 +253,49 @@ export default function AdminClientList() {
                                 }}
                                 style={styles.profileImg}
                             />
-                            <View style={styles.clientTextContainer}>
-                                <Text style={styles.clientText}>
-                                    {client.username}{" "}
-                                </Text>
-                                <Text style={styles.clientText}>
-                                    {client.email}
-                                </Text>
-                                <Text style={styles.clientText}>
-                                    {client.phoneNumber}
-                                </Text>
-                            </View>
-                            <View style={styles.clientAptsContainer}>
-                                {client.appointments.length ? (
-                                    client.appointments.length > 1 ? (
-                                        <Text style={styles.upcomingAptText}>
-                                            {client.appointments.length}{" "}
-                                            Upcoming Appointments
+                            <View style={styles.clientInfo}>
+                                <View style={styles.clientTextContainer}>
+                                    <Text style={styles.clientText}>
+                                        {client.username}{" "}
+                                    </Text>
+                                    <Text style={styles.clientText}>
+                                        {client.phoneNumber}
+                                    </Text>
+                                    <TouchableOpacity>
+                                        <Text style={styles.textLink}>
+                                            View More
                                         </Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={styles.clientAptsContainer}>
+                                    {client.appointments.length ? (
+                                        client.appointments.length > 1 ? (
+                                            <Text
+                                                style={styles.upcomingAptText}
+                                            >
+                                                {client.appointments.length}{" "}
+                                                Upcoming Appointments
+                                            </Text>
+                                        ) : (
+                                            <Text
+                                                style={styles.upcomingAptText}
+                                            >
+                                                {" "}
+                                                1 Upcoming Appointment
+                                            </Text>
+                                        )
                                     ) : (
                                         <Text style={styles.upcomingAptText}>
-                                            {" "}
-                                            1 Upcoming Appointment
+                                            No Upcoming Appointments
                                         </Text>
-                                    )
-                                ) : (
-                                    <Text style={styles.upcomingAptText}>
-                                        No Upcoming Appointments
-                                    </Text>
-                                )}
+                                    )}
 
-                                <TouchableOpacity>
-                                    <Text style={styles.upcomingAptManage}>
-                                        Manage
-                                    </Text>
-                                </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Text style={styles.textLink}>
+                                            Manage
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
                         </View>
                     );
@@ -117,6 +314,7 @@ const styles = StyleSheet.create({
     },
     addClientButton: { alignItems: "center", padding: 15 },
     addClientFont: { fontWeight: "600" },
+
     clientListContainer: { width: "100%" },
     client: {
         borderWidth: 0.2,
@@ -124,22 +322,70 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         padding: 5,
-        justifyContent: "space-evenly",
     },
     profileImg: {
         width: 75,
         height: 75,
         borderRadius: 100,
     },
+    clientInfo: {
+        paddingLeft: 20,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        width: "70%",
+    },
     clientTextContainer: { paddingLeft: "5%" },
-    clientAptsContainer: {},
+    clientAptsContainer: { padding: 1 },
     clientText: { padding: 1 },
-    upcomingAptManage: {
+    textLink: {
         textAlign: "center",
         color: "deepskyblue",
         fontWeight: "600",
+        padding: 1,
     },
     upcomingAptText: {
         maxWidth: 165,
+    },
+    modalContainer: {
+        margin: 20,
+        backgroundColor: "whitesmoke",
+        paddingHorizontal: 50,
+        paddingTop: 60,
+        height: "100%",
+        width: "100%",
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingBottom: 175,
+        textAlign: "center",
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+    },
+    button: {
+        backgroundColor: "deepskyblue",
+        textAlign: "center",
+        borderRadius: 7,
+        padding: 10,
+        margin: 5,
+        elevation: 2,
+        width: 75,
+    },
+    buttonText: { color: "white", fontWeight: "600", textAlign: "center" },
+    buttonContainer: { flexDirection: "row", justifyContent: "center" },
+    input: {
+        backgroundColor: "white",
+        borderColor: "black",
+        height: 40,
+        padding: 10,
+        borderRadius: 4,
+        margin: 10,
     },
 });
